46a13506ff118b8aa849d70fbd77548539db8d2d
Heynner_Velasco <hvelasco01@ucatolica.edu.co>
2026-02-19 15:18:21 -0500

    Decimal a romano

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -118,6 +118,7 @@ class Conversion:
         for digito in reversed(binario):
             decimal += int(digito) * (2 ** potencia)
             potencia += 1
+        return decimal
         pass
     
     def decimal_a_romano(self, numero):
@@ -134,6 +135,27 @@ class Conversion:
             decimal_a_romano(9) -> "IX"
             decimal_a_romano(1994) -> "MCMXCIV"
         """
+        valores =[
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
+        romano=""
+        for valor,simbolo in valores:
+            while numero >= valor:
+                romano += simbolo
+                numero -= valor
+        return romano
         pass
     
     def romano_a_decimal(self, romano):
