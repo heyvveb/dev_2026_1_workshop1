98dc6e0dffb136b609a8aecfd950971b157ff412
Heynner_Velasco <hvelasco01@ucatolica.edu.co>
2026-02-22 19:42:13 -0500

    Generar primos

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -83,8 +83,8 @@ class Magic:
         """
         primos=[]
         p=0
-        for j in range(n):
-            for i in range(n):
+        for j in range(2,n+1):
+            for i in range(1,j+1):
                 if j%i==0:
                     p+=1
                 if p==2:
