c6f9ff89b6fdcd3dd53a81f7c24945e029e3c707
Heynner_Velasco <hvelasco01@ucatolica.edu.co>
2026-02-22 19:37:33 -0500

    Secuencia fibonacci 3

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -63,7 +63,7 @@ class Magic:
         p=0
         if n<0:
             return False
-        for i in range(1,n):
+        for i in range(1,n+1):
             if n%i==0:
                 p+=1
         if p==2:
