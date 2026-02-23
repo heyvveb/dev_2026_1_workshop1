8860c8ccf62b85cb1cd19d57c25f674ab75fac24
Heynner_Velasco <hvelasco01@ucatolica.edu.co>
2026-02-22 20:27:38 -0500

    Factorial 5

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -142,10 +142,10 @@ class Magic:
         Returns:
             int: El factorial de n
         """
-        fac=0
+        fac=1
         if n<2:
             return 1
-        for i in range(1,n):
+        for i in range(2,n+1):
             fac*=i
         return fac
         pass
