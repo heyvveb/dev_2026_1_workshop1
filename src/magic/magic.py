8c1a09af40e342cafaf2515be6d26f80b5967252
Heynner_Velasco <hvelasco01@ucatolica.edu.co>
2026-02-22 19:38:27 -0500

    Secuencia fibonacci 4

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -61,7 +61,7 @@ class Magic:
             bool: True si n es primo, False en caso contrario
         """
         p=0
-        if n<0:
+        if n<2:
             return False
         for i in range(1,n+1):
             if n%i==0:
